85959416153aa3f5a48fe3b8ec3f1409d023dedc
Robert Erb <bob.erb@gmail.com>
2016-12-18 19:07:47 -0500

    Refactor some constants

--- test_tv/apps/results/models.py
+++ test_tv/apps/results/models.py
@@ -8,24 +8,28 @@ class Test(models.Model):
 
 class TestResult(models.Model):
 
+    FAILURE = "failure"
+    ERROR = "error"
+    SUCCESS = "success"
+
     test = models.ForeignKey(Test, related_name="results")
     status = models.CharField(max_length=24, db_index=True)
 
     @classmethod
     def get_num_failures(cls):
-        return cls.objects.filter(status="failure").count()
+        return cls.objects.filter(status=cls.FAILURE).count()
 
     @classmethod
     def get_num_errors(cls):
-        return cls.objects.filter(status="error").count()
+        return cls.objects.filter(status=cls.ERROR).count()
 
     @classmethod
     def get_num_successes(cls):
-        return cls.objects.filter(status="success").count()
+        return cls.objects.filter(status=cls.SUCCESS).count()
 
     @classmethod
     def get_num_left(cls):
         return cls.objects.filter(status="").count()
 
     def fail(self):
-        self.status = "failure"
+        self.status = self.FAILURE
